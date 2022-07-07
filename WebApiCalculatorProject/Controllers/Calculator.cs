852e2096433d199a683e9f12905c50c5a0403ecc
Sifiso Makhaye <sifiso.makhaye@sage.com>
2022-07-07 18:17:25 +0200

    Add project files.

---
+++ WebApiCalculatorProject/Controllers/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCalculatorProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Calculator : ControllerBase
+    {
+        [HttpGet]
+        // Addition
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpGet]
+        // Subtraction 
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        [HttpGet]
+        // Multliply
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        [HttpGet]
+        // Divide
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "default";
+        }
+    }
+}
